65a4005041eddc967a25b8953287da34e194f0fb
Styl1st <151127732+Styl1st@users.noreply.github.com>
2025-12-06 18:17:08 +0100

    modification îcone

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,20 @@
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+
+import logoWhite from './assets/newave/logo.png'
+
+const setFavicon = (url: string, type = 'image/png') => {
+  let link = document.querySelector("link[rel~='icon']") as HTMLLinkElement | null
+  if (!link) {
+    link = document.createElement('link')
+    link.rel = 'icon'
+    document.head.appendChild(link)
+  }
+  link.type = type
+  link.href = url
+}
+setFavicon(logoWhite);
 
 createRoot(document.getElementById("root")!).render(<App />);
